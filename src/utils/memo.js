3e5d06d0ea2d6b535fbcef3cc9b9f01b263851d9
jeff.miller <hello@jamiller.me>
2019-09-17 17:32:55 -0400

    moved memo to own file

---
+++ src/utils/memo.js
@@ -0,0 +1,29 @@
+import { isPromise } from './shared'
+
+const cacheStore = new WeakMap()
+
+const memo = (fn, { cache = new Map(), cacheKey, cacheDepth = 3 } = {}) => {
+  return function memoized (...args) {
+    const key = cacheKey(...args)
+
+    if (cache.has(key)) return cache.get(key).data
+
+    // eslint-disable-next-line no-invalid-this
+    const cacheItem = fn.apply(this, ...args)
+
+    cache.set(key, {
+      data: cacheItem,
+      depth: cacheDepth
+    })
+
+    if (isPromise(cacheItem)) {
+      cacheItem.catch(() => cache.delete(key))
+    }
+
+    cacheStore.set(memoized, cache)
+
+    return cacheItem
+  }
+}
+
+export default memo
